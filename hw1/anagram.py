df0369e6c06672c2a0989c306d508579950ac26b
Joel Mathew <joelmathew.mec@gmail.com>
2016-01-18 13:43:39 -0800

    First part.

--- hw1/anagram.py
+++ hw1/anagram.py
@@ -1,2 +1,21 @@
+import sys
 
+if len(sys.argv) < 2:
+    print "No input text given."
+    exit()
 
+def permute(text, res=''):
+    if len(text) == 1:
+        result.append(res+text)
+    for x in text:
+        text_copy = text.replace(x, '')
+        permute(text_copy, res+x)
+
+result = []
+wfile = open('anagram_out.txt', 'w')
+permute(sys.argv[1])
+output = sorted(result, key=str.lower)
+
+for r in output:
+    wfile.write(r + '\n')
+wfile.close()
