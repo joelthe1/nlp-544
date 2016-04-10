489ae23cfe33a9436fe0fc55f0c0102d8d0699cb
Joel Mathew <joelmathew.mec@gmail.com>
2016-04-10 15:33:33 -0700

    Read in.

--- hw6/hmmlearn3.py
+++ hw6/hmmlearn3.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import re
+from decimal import *
 
 mapping = {}
 path = sys.argv[1]
@@ -8,51 +9,55 @@ if not os.path.exists(path):
     print('Error! Given path is not a valid directory.')
     exit()
 
+def separate(token):
+    patt = re.compile(r'(.*?)\/([A-Z0-9]{2})')
+    res = patt.match(token)
+    #TODO: if res: for safety
+    word = res.group(1)
+    tag = res.group(2)
+    return {'word': word, 'tag': tag}
+
 def tokenize():
     print(path)
-    pat = re.compile(r'(.*?)\/([A-Z0-9]{2})')
     with open(path, 'r') as rfile:
         for line in rfile.readlines():
-#            print line
+            print(line)
+            if not line.strip():
+                continue
             line_segs = line.strip().split(' ')
-            res = pat.match(segs)
-            #TODO: if res: for safety
-            word = res.group(1)
-            tag = res.group(2)
-            if tag not in mapping:
-                mapping[tag] = {}
-                mapping[tag]['tags'] = {'<~s~>': 1}
-                mapping[tag]['words'] = {word: 1}
-                mapping[tag]['trans_denom'] = 1
-                mapping[tag]['emmis_denom'] = 1
-            prev = (word, tag)
+            prev = {'tag': '<~s~>'}
 
-            for segs in line_segs[1:-1]:
-                res = pat.match(segs)
-                print(line_segs)
-                word = res.group(1)
-                tag = res.group(2)
-                mapping[tag]['trans_denom'] += 1
-                mapping[tag]['emmis_denom'] += 1
-                if prev[1] not in mapping[tag]['tags']:
-                    mapping[tag]['tags'][prev[1]] = 1
-                else:
-                    mapping[tag]['tags'][prev[1]] += 1
+            for seg in line_segs:
+                token = separate(seg)
+                insert(prev, token)
+                prev = token
+    print mapping
 
-                if prev[2] not in mapping[tag]['words']:
-                    mapping[tag]['words'] = 1
-                else:
-                    mapping[tag]['words'] += 1
-                prev = (word, tag)
+def insert(prev, curr):
+    global mapping
+    if curr['tag'] not in mapping:
+        mapping[curr['tag']] = {}
+        mapping[curr['tag']]['tags'] = {prev['tag']: 1}
+        mapping[curr['tag']]['words'] = {curr['word']: 1}
+        mapping[curr['tag']]['emmis_denom'] = 1    
+    else:
+        if prev['tag'] in mapping[curr['tag']]['tags']:
+            mapping[curr['tag']]['tags'][prev['tag']] += 1
+        else:
+            mapping[curr['tag']]['tags'][prev['tag']] = 1
 
-                
+        if curr['word'] in mapping[curr['tag']]['words']:
+            mapping[curr['tag']]['words'][curr['word']] += 1
+        else:
+            mapping[curr['tag']]['words'][curr['word']] = 1
+        mapping[curr['tag']]['emmis_denom'] += 1
 
-#    for root, sdirs, files in os.walk(path):
-#        for filename in files:
-#            file_path = os.path.join(root, filename)
-#            if filename.startswith('.') or filename == 'LICENSE' or filename == 'README.txt':
-#                continue
+    if prev['tag'] not in mapping:
+        mapping[prev['tag']] = {}
 
-            
-            
+    if 'trans_denom' not in mapping[prev['tag']]:
+        mapping[prev['tag']]['trans_denom'] = 1
+    else:
+        mapping[prev['tag']]['trans_denom'] += 1
+
 tokenize()
