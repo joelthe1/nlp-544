beae6a0a243b1b6f918918bd0ef7a96a99a3e56b
Joel Mathew <joelmathew.mec@gmail.com>
2016-04-12 02:19:10 -0700

    Adding init for algo.

--- hw6/hmmdecode3.py
+++ hw6/hmmdecode3.py
@@ -1,25 +1,54 @@
 import pickle
 import numpy as np
+import sys
+import os
 
 path = sys.argv[1]
 if not os.path.exists(path):
     print('Error! Given path is not a valid directory.')
     exit()
 
-p = ''
 with open('hmmmodel.txt', 'rb') as rfile:
     global p
     p = pickle.load(rfile)
-    print(p)
+#    print(p)
 
+start_state = '<~s~>'
 tag_set = p.keys()
-tag_set_size = len(tag_set) - 1
+tag_set_size = len(tag_set)
+tag_map = {}
+for index, tag in enumerate(tag_set):
+    tag_map[tag] = index
+
+print(tag_map)
 
 with open(path, 'r') as rfile:
     for line in rfile.readlines():
         if not line.strip():
             continue
         line_segs = line.strip().split(' ')
+        print(line_segs)
+        T = len(line_segs)
+        print(T)
+        prob = np.zeros((tag_set_size, T), dtype=np.float64)
+        backp = np.zeros((tag_set_size, T), dtype=np.float64)
+        
+        # Initialization step
+        for tag in tag_set:
+            print(tag)
+            obs_p = 1
+            if line_segs[0] in p[tag]['words']:
+                obs_p = p[tag]['words'][line_segs[0]].log10()
+            prob[tag_map[tag]][0] = p[tag]['tags'][start_state].log10() + obs_p
 
-prob = np.zero((tag_set_size, tag_set_size), dtype=float64)
-backp = []
+        # Filling the rest
+        for t in range(1, T):
+            for tag1 in tag_set:
+                for tag2 in tag_set:
+                obs_p = 1
+                if line_segs[t] in p[tag]['words']:
+                    obs_p = p[tag]['words'][line_segs[t]].log10()
+                prob[tag_map[tag]][t] = p[tag]['tags'][tag].log10() + obs_p
+               
+
+        print(prob)
