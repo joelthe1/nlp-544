7ff14b003ff3e335cd8c319ea5bd657756fd6c86
Joel Mathew <joelmathew.mec@gmail.com>
2016-05-04 15:35:23 -0700

    Fixes.

--- hw8/calculatebleu3.py
+++ hw8/calculatebleu3.py
@@ -32,6 +32,7 @@ def openup(path):
         return data
 
 def ngramize_and_count(sentence, n=1):
+    sentence = sentence.lower()
     tokens = sentence.strip().split(' ')
     ngrams = []
     for x in range(len(tokens)-n+1):
@@ -63,29 +64,41 @@ def mod_precision_counts(candidate_data, ref_data, n):
         matches = {}
         ref_counts = []
         sentence_sum = 0
+        print('\n candidate:', sentence)
         candidate_counts = ngramize_and_count(sentence, n)
+        print('candidate_counts:', candidate_counts)
         if candidate_counts:
             denom += functools.reduce(lambda x,y:x+y, candidate_counts.values())
         else:
             denom += 1
+        print('denom is:', denom)
         cand_len = len(sentence.strip().split(' '))
+        print(cand_len, '\n')
         c += cand_len
         temp_r = float('inf')
         for refs in ref_data:
             l = len(refs[index].strip().split(' '))
+            print('ref_len', l)
             if temp_r > abs(l-cand_len):
                 temp_r = l
+            print('temp_r:', temp_r)
             ref_counts.append(ngramize_and_count(refs[index], n))
+#            print('ref_counts:', ref_counts)
         r += temp_r
-
+        print(len(candidate_counts.keys()))
         for ngram in candidate_counts.keys():
+#            print(ngram)
             cand_count = candidate_counts[ngram]
+#            print('cand:',cand_count)
             val = 0
             for ref in ref_counts:
                 if ngram in ref:
                     if val < ref[ngram]:
                         val = ref[ngram]
+                    print('ref',ref[ngram])
             clipped_count = min(cand_count, val)
+            print('clipped', clipped_count)
+            print()
             sentence_sum += clipped_count
         numerator += sentence_sum
 #        print(candidate_counts, ref_counts)
@@ -100,8 +113,26 @@ if __name__ == '__main__':
     print(len(ref_data))
     candidate_data = readin(candidate_path)
     print(len(candidate_data))
-
+#    print()
+#    print(candidate_data)
+#    print()
+#    print(ref_data[0])
+#    print()
+#    print(ref_data[1])
     mod_p_values = []
+
+#    for i, v in enumerate(candidate_data):
+#        print()
+#        print('cand:',v)
+#        print('cand_count:', ngramize_and_count(v, 2))
+#        print()
+#        print('ref1:',ref_data[0][i])
+#        print('ref1_count:', ngramize_and_count(ref_data[0][i], 2))
+#        print()
+#        print('ref2',ref_data[1][i])
+#        print('ref2_count:', ngramize_and_count(ref_data[1][i], 2))
+#        print('*********')
+#    exit()
 
     # N in the paper.
     N = 4
@@ -113,7 +144,7 @@ if __name__ == '__main__':
     r,c = mod_p_values[0][0], mod_p_values[0][1]
 
     # Calculate BP
-    bp = 1-(r/c)
+    bp = 1.0-(r/c)
     
     print(bp)
 
@@ -121,7 +152,7 @@ if __name__ == '__main__':
     # Geometric avg. of Pn
     for p in mod_p_values:
         gavg += log(p[2])
-    print(gavg/4)
+    print(gavg/N)
     logbleu = min(bp, 0) + gavg/N
     print('logbleu is', logbleu)
     bleu = exp(logbleu)
